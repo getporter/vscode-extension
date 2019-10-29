14e0fcb186712153dcfe62dfa59895a0148c08bc
itowlson <ivan.towlson@microsoft.com>
2019-10-29 21:14:41 +1300

    Interim commit

--- src/commands/movestep.ts
+++ src/commands/movestep.ts
@@ -2,14 +2,14 @@ import * as vscode from 'vscode';
 
 import * as ast from '../porter/ast';
 
-const EMPTY_DISPOSABLE = new vscode.Disposable(() => undefined);
-
 abstract class MoveStepDirection {
     abstract allowsMove(action: ast.PorterActionYAML, stepIndex: number): boolean;
     abstract readonly utmost: string;
     abstract readonly name: string;
     abstract moverIndex(stepIndex: number): number;
     abstract moveTargetIndex(stepIndex: number): number;
+    abstract cursorOffsetSizeStepIndex(stepIndex: number): number;
+    abstract yFactor: number;
 
     static readonly UP: MoveStepDirection = {
         allowsMove(_action, stepIndex) {
@@ -18,7 +18,9 @@ abstract class MoveStepDirection {
         utmost: 'top',
         name: 'up',
         moverIndex(stepIndex) { return stepIndex; },
-        moveTargetIndex(stepIndex) { return stepIndex - 1; }
+        moveTargetIndex(stepIndex) { return stepIndex - 1; },
+        cursorOffsetSizeStepIndex(stepIndex) { return stepIndex - 1; },
+        yFactor: -1
     };
 
     static readonly DOWN: MoveStepDirection = {
@@ -28,60 +30,65 @@ abstract class MoveStepDirection {
         utmost: 'bottom',
         name: 'down',
         moverIndex(stepIndex) { return stepIndex + 1; },
-        moveTargetIndex(stepIndex) { return stepIndex; }
+        moveTargetIndex(stepIndex) { return stepIndex; },
+        cursorOffsetSizeStepIndex(stepIndex) { return stepIndex + 1; },
+        yFactor: 1
     };
 }
 
-export function moveStepUp(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]): vscode.Disposable {
-    return moveStep(editor, edit, MoveStepDirection.UP);
+export function moveStepUp(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]): void {
+    moveStep(editor, edit, MoveStepDirection.UP);
 }
 
-export function moveStepDown(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]): vscode.Disposable {
-    return moveStep(editor, edit, MoveStepDirection.DOWN);
+export function moveStepDown(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]): void {
+    moveStep(editor, edit, MoveStepDirection.DOWN);
 }
 
-function moveStep(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, direction: MoveStepDirection): vscode.Disposable {
+function moveStep(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, direction: MoveStepDirection): void {
     const document = editor.document;
     if (!document.uri.fsPath.toLowerCase().endsWith('porter.yaml')) {
         vscode.window.showErrorMessage('This command requires an open editor containing the porter.yaml to insert into');
-        return EMPTY_DISPOSABLE;
+        return;
     }
     const manifest = ast.parse(document.getText());
     if (!manifest) {
         vscode.window.showErrorMessage('Unable to parse open porter.yaml file: please fix any errors and try again');
-        return EMPTY_DISPOSABLE;
+        return;
     }
 
-    const cursorLocation = locateCursorInActions(manifest, editor.selection.active.line);
+    const originalCursorPosition = editor.selection.active;
+    const cursorLocation = locateCursorInActions(manifest, originalCursorPosition.line);
     if (!cursorLocation) {
         vscode.window.showErrorMessage('This command requires the cursor to be on the step you want to move');
-        return EMPTY_DISPOSABLE;
+        return;
     }
 
     const [action, stepIndex] = cursorLocation;
 
     if (!direction.allowsMove(action, stepIndex)) {
         vscode.window.showInformationMessage(`This step is already at the ${direction.utmost} and cannot be moved ${direction.name}`);
-        return EMPTY_DISPOSABLE;
+        return;
     }
 
-    // TODO: MOVE IT MOVE IT
     const movingStepIndex = direction.moverIndex(stepIndex);
     const moveTargetIndex = direction.moveTargetIndex(stepIndex);
+    const cursorOffsetSizeStepIndex = direction.cursorOffsetSizeStepIndex(stepIndex);
 
     const movingStep = action.steps[movingStepIndex];
     const moveTargetStep = action.steps[moveTargetIndex];
+    const cursorOffsetSizeStep = action.steps[cursorOffsetSizeStepIndex];
 
     const movingStepRange = new vscode.Range(movingStep.startLine, 0, movingStep.endLine + 1, 0);
     const movingStepText = editor.document.getText(movingStepRange);
     const targetPosition = new vscode.Position(moveTargetStep.startLine, 0);
+    const cursorTranslateLines = (cursorOffsetSizeStep.endLine - cursorOffsetSizeStep.startLine + 1) * direction.yFactor;
+    const newCursorPosition = originalCursorPosition.translate(cursorTranslateLines, 0);
 
     edit.delete(movingStepRange);
     edit.insert(targetPosition, movingStepText);
+    editor.selection = new vscode.Selection(newCursorPosition, newCursorPosition);
 
-    // TODO: position cursor at the location where it originally was within the moved step
-
-    return EMPTY_DISPOSABLE;
+    return;
 }
 
 function locateCursorInActions(manifest: ast.PorterManifestYAML, line: number): [ast.PorterActionYAML, number] | undefined {
