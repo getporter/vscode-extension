da3626ce980da097a215a12c6a9c474cc454f34f
itowlson <ivan.towlson@microsoft.com>
2019-11-06 09:43:00 +1300

    Create parameter defaulting to selection

---
+++ src/commands/parameterise.ts
@@ -0,0 +1,74 @@
+import * as vscode from 'vscode';
+
+import * as ast from '../porter/ast';
+import { findPorterManifestDocument } from '../utils/manifestselection';
+
+export async function parameteriseSelection(): Promise<void> {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        await vscode.window.showErrorMessage('This command requires an open text editor');
+        return;
+    }
+
+    const selection = editor.selection;
+    if (selection.start.isEqual(selection.end)) {
+        await vscode.window.showErrorMessage('This command requires you to select some text first');
+        return;
+    }
+
+    const activeDocument = editor.document;
+
+    const porterManifestDocument = await findPorterManifestDocument(activeDocument);
+    if (!porterManifestDocument) {
+        await vscode.window.showErrorMessage('This command requires a folder containing a porter.yaml file');
+        return;
+    }
+
+    const manifest = ast.parse(porterManifestDocument.getText());
+    if (!manifest) {
+        await vscode.window.showErrorMessage('This command requires the porter.yaml file to be valid; yours currently has errors');
+        return;
+    }
+
+    const text = editor.document.getText(selection);
+
+    const wsedit = makeParameterisationEdits(porterManifestDocument, manifest, text);
+
+    const edited = await vscode.workspace.applyEdit(wsedit);
+    if (!edited) {
+        await vscode.window.showErrorMessage('Unable to extract the selection to a parameter');
+        return;
+    }
+
+    // TODO: consider switching to porter.yaml and putting the cursor on the new parameter
+    // (or we could use an insertSnippet though that doesn't coordinate with changes to the
+    // source document if we make them)
+}
+
+function makeParameterisationEdits(porterManifest: vscode.TextDocument, manifest: ast.PorterManifestYAML, sourceText: string): vscode.WorkspaceEdit {
+    const edit = new vscode.WorkspaceEdit();
+
+    const name = 'TO BE WORKED OUT';
+
+    const parameterDefinitionText = `  - name: ${name}\n    default: ${sourceText}\n    description: TO BE WORKED OUT\n`;
+    if (manifest.parameters) {
+        // insert the text after the section (or, for now, as the top line of the section)
+        const position = new vscode.Position(manifest.parameters.startLine + 1, 0);
+        edit.insert(porterManifest.uri, position, parameterDefinitionText);
+    } else {
+        // prefix with `parameters:\n` and stick it any old where
+        const parametersSectionText = `\nparameters:\n${parameterDefinitionText}`;
+        const position = endPosition(porterManifest);
+        edit.insert(porterManifest.uri, position, parametersSectionText);
+    }
+
+    // TODO: if the active document is porter.yaml, replace the selected text with {{ bundle.paraneters.${name} }}
+
+    return edit;
+}
+
+function endPosition(document: vscode.TextDocument): vscode.Position {
+    const lastLineIndex = document.lineCount - 1;
+    const lastLine = document.lineAt(lastLineIndex);
+    return lastLine.range.end;
+}
---
+++ src/utils/manifestselection.ts
@@ -0,0 +1,46 @@
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+import './array';
+import { fs } from '../utils/fs';
+
+export async function findPorterManifestDirectory(): Promise<string | undefined> {
+    const editor = vscode.window.activeTextEditor;
+    if (editor && editor.document.uri.fsPath.indexOf('porter.yaml') >= 0) {
+        return path.dirname(editor.document.uri.fsPath);
+    }
+
+    const folders = await porterFolders();
+
+    if (!folders || folders.length === 0) {
+        return undefined;
+    }
+
+    return folders[0];
+}
+
+export async function findPorterManifestDocument(activeDocument: vscode.TextDocument): Promise<vscode.TextDocument | undefined> {
+    if (activeDocument.uri.fsPath.includes('porter.yaml')) {
+        return activeDocument;
+    }
+
+    const folders = await porterFolders();
+    if (!folders || folders.length === 0) {
+        return undefined;
+    }
+
+    const containingFolder = folders.find((f) => activeDocument.uri.fsPath.startsWith(f));
+    const bestFolder = containingFolder || folders[0];
+
+    return vscode.workspace.openTextDocument(path.join(bestFolder, 'porter.yaml'));
+}
+
+async function porterFolders(): Promise<readonly string[]> {
+    const folders = vscode.workspace.workspaceFolders;
+    if (!folders || folders.length === 0) {
+        return [];
+    }
+
+    const matches = await folders.filterAsync<vscode.WorkspaceFolder>(async (f) => await fs.exists(path.join(f.uri.fsPath, 'porter.yaml')));
+    return matches.map((f) => f.uri.fsPath);
+}
