5d524f939dbf22aa589fa26e88e5fe215cda0fc3
itowlson <ivan.towlson@microsoft.com>
2021-04-13 12:54:09 +1200

    A bit more installation info
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>

--- src/explorer/installation/installation-explorer.ts
+++ src/explorer/installation/installation-explorer.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 
 import * as porter from '../../porter/porter';
+import { Installation } from '../../porter/porter.objectmodel';
 import { Shell } from '../../utils/shell';
 import { ExplorerErrorNode } from '../errornode';
 import { Node } from '../node';
@@ -28,18 +29,25 @@ export class InstallationExplorer implements vscode.TreeDataProvider<Installatio
         if (!installations.succeeded) {
             return [new ExplorerErrorNode(installations.error[0])];
         }
-        return installations.result.map((i) => new InstallationNode(i.Name));
+        return installations.result.map((i) => new InstallationNode(i));
     }
 }
 
 class InstallationNode implements Node {
     readonly kind = 'installation' as const;
-    constructor(private readonly name: string) {}
+    constructor(private readonly installation: Installation) {}
     getTreeItem(): vscode.TreeItem {
-        return new vscode.TreeItem(this.name);
+        const treeItem = new vscode.TreeItem(this.installation.Name);
+        treeItem.tooltip = `Last action: ${this.installation.Action} (${this.installation.Status})\nat: ${displayTime(this.installation.Modified)}`;
+        return treeItem;
     }
 }
 
 export type InstallationExplorerTreeNode =
     InstallationNode |
     ExplorerErrorNode;
+
+function displayTime(timeString: string): string {
+    const time = new Date(timeString);
+    return `${time.toLocaleDateString()} ${time.toLocaleTimeString()}`;  // TODO: this is a bit rubbish
+}
